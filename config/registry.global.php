9ef68623ad3e99f4179e7207755f4a48a04874c4
Curtis Kelsey <kelseyc@truman.edu>
2016-09-14 14:39:15 -0500

    Added first round of assets

---
+++ config/registry.global.php
@@ -0,0 +1,39 @@
+<?php
+use AssetRegistryPlugin\Constants;
+
+return array(
+    'asset-registry' => array(
+        'javascript' => array(
+            Constants::ACE_KEY => 'bower_components/ace/lib/ace.js',
+            Constants::BACKBONE_KEY => 'bower_components/backbone/backbone.js',
+            Constants::BOOTSTRAP_KEY =>  'bower_components/bootstrap/dist/js/bootstrap.js',
+            Constants::CHARTJS_KEY => 'bower_components/chartjs/src/chart.js',
+            Constants::CKEDITOR_KEY => 'bower_components/ckeditor/ckeditor.js',
+            Constants::DATATABLES_KEY => 'bower_components/js/jquery.dataTables.js',
+            Constants::DATEJS_KEY => 'bower_components/build/date.js',
+            Constants::FACEBOOK_KEY => '//connect.facebook.net/en_US/all.js',
+            Constants::FANCYBOX_KEY => 'bower_components/fancybox/source/jquery.fancybox.js',
+            Constants::FULLCALENDAR_KEY => 'bower_components/fullcalendar/dist/fullcalendar.js',
+            Constants::INFINITY_KEY => 'bower_components/infinity/build/inifnity.js',
+            Constants::JWPLAYER_KEY => 'bower_components/jwplayer-official/src/js/jwplayer.js',
+            Constants::LIGHTBOX_KEY => 'bower_components/lightbox2/dist/js/lightbox.js',
+            Constants::MAGNIFIC_POPUP_KEY => 'bower_components/magnific-popup/dist/jquery.magnific-popup.js',
+            Constants::MODERNIZR_KEY => 'bower_components/modernizr/bin/modernizr',
+            Constants::MOMENT_KEY => 'bower_components/moment/src/moment.js',
+            Constants::MUSTACHE_KEY => 'bower_components/mustache.js/mustache.js',
+            Constants::PRETTY_PHOTO_KEY => 'bower_components/prettyphoto/js/jquery.prettyPhoto.js',
+            Constants::RAPHAEL_KEY => 'bower_components/raphael/raphael.js',
+            Constants::SLICK_KEY => 'bower_components/slick-carousel/slick/slick.js',
+            Constants::UNDERSCORE_KEY => 'bower_components/underscore/underscore.js'
+        ),
+        'stylesheet' => array(
+            Constants::BOOTSTRAP_KEY =>  'bower_components/bootstrap/dist/css/bootstrap.css',
+            Constants::FANCYBOX_KEY => 'bower_components/fancybox/source/jquery.fancybox.css',
+            Constants::FULLCALENDAR_KEY => 'bower_components/fullcalendar/dist/fullcalendar.css',
+            Constants::LIGHTBOX_KEY => 'bower_components/lightbox2/dist/css/lightbox.css',
+            Constants::MAGNIFIC_POPUP_KEY => 'bower_components/magnific-popup/dist/magnific-popup.css',
+            Constants::PRETTY_PHOTO_KEY => 'bower_components/prettyphoto/css/prettyPhoto.css',
+            Constants::SLICK_KEY => 'bower_components/slick-carousel/slick/slick-theme.css',
+        )
+    )
+);
